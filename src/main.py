04e0c27c5e35719ec8ac6fd5eaa29e58e684ccfe
Gustav Dahmén <gd.dahmen@gmail.com>
2024-01-21 12:27:04 +0100

    first commit with structure and set up

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# Initialize the src directory as a Python package
+# src/main.py
+
+def getGroups():
+    # My main logic to get groups in the park
+    pass
+
+if __name__ == "main": 
+    getGroups()
